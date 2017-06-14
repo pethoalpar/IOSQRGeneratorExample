0f09feb595267672cacb362fa298c05f94934b04
pethoalpar <pethoalpar@gmail.com>
2017-06-14 23:32:11 +0300

    First commit

--- QRGeneratorExample/ViewController.swift
+++ QRGeneratorExample/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var editTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func generate(_ sender: Any) {
+        let image = generateQrImage(from: editTextField.text!)
+        imageView.image = image
+    }
+    
+    func generateQrImage(from text: String) -> UIImage?{
+        let data = text.data(using: String.Encoding.ascii)
+        
+        if let filter = CIFilter(name: "CIQRCodeGenerator"){
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 3, y:3)
+            if let output = filter.outputImage?.applying(transform){
+                return UIImage(ciImage: output)
+            }
+        }
+        return nil
+    }
 
 }
 
